546cee89aba963f0d6646c3830ed1f7a22958a93
Morten Sørensen <moso@aidgency.dk>
2018-04-17 14:43:46 +0200

    Fixed lacking support for xterm api

--- index.js
+++ index.js
@@ -9,26 +9,34 @@ exports.decorateConfig = (config) => {
     }
 
     return Object.assign({}, config, {
-        termCSS: `
-            ${config.termCSS || ''}
+        css: `
+            ${config.css || ''}
+            .xterm-viewport::-webkit-scrollbar-button,
             ::-webkit-scrollbar-button {
                 width: 0;
                 height: 0;
                 display: none;
             }
+            .xterm-viewport::-webkit-scrollbar-corner
             ::-webkit-scrollbar-corner {
                 background-color: transparent;
             }
+            .xterm-viewport::-webkit-scrollbar,
             ::-webkit-scrollbar {
                 width: 4px;
                 height: 4px;
             }
-            ::-webkit-scrollbar-track, ::-webkit-scrollbar-thumb {
+            .xterm-viewport::-webkit-scrollbar-track,
+            .xterm-viewport::-webkit-scrollbar-thumb,
+            ::-webkit-scrollbar-track,
+            ::-webkit-scrollbar-thumb {
                 -webkit-border-radius: 8px;
             }
+            .xterm-viewport::-webkit-scrollbar-track,
             ::-webkit-scrollbar-track {
                 background-color: ${colors.dark};
             }
+            .xterm-viewport::-webkit-scrollbar-thumb,
             ::-webkit-scrollbar-thumb {
                 background-color: ${colors.light};
                 -webkit-box-shadow: none;
