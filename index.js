eb068e0313c9543db8729a9fe0aaa19b763e6a84
Morten Sørensen <moso@aidgency.dk>
2018-04-17 15:06:36 +0200

    Removed support for Hyper 1.4.8

--- index.js
+++ index.js
@@ -11,33 +11,26 @@ exports.decorateConfig = (config) => {
     return Object.assign({}, config, {
         css: `
             ${config.css || ''}
-            .xterm-viewport::-webkit-scrollbar-button,
-            ::-webkit-scrollbar-button {
+            .xterm-viewport::-webkit-scrollbar-button {
                 width: 0;
                 height: 0;
                 display: none;
             }
-            .xterm-viewport::-webkit-scrollbar-corner
-            ::-webkit-scrollbar-corner {
+            .xterm-viewport::-webkit-scrollbar-corner {
                 background-color: transparent;
             }
-            .xterm-viewport::-webkit-scrollbar,
-            ::-webkit-scrollbar {
+            .xterm-viewport::-webkit-scrollbar {
                 width: 4px;
                 height: 4px;
             }
             .xterm-viewport::-webkit-scrollbar-track,
-            .xterm-viewport::-webkit-scrollbar-thumb,
-            ::-webkit-scrollbar-track,
-            ::-webkit-scrollbar-thumb {
+            .xterm-viewport::-webkit-scrollbar-thumb {
                 -webkit-border-radius: 8px;
             }
-            .xterm-viewport::-webkit-scrollbar-track,
-            ::-webkit-scrollbar-track {
+            .xterm-viewport::-webkit-scrollbar-track {
                 background-color: ${colors.dark};
             }
-            .xterm-viewport::-webkit-scrollbar-thumb,
-            ::-webkit-scrollbar-thumb {
+            .xterm-viewport::-webkit-scrollbar-thumb {
                 background-color: ${colors.light};
                 -webkit-box-shadow: none;
             }
